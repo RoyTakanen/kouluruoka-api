bf69a7406134f632eae7d0f2547eadff7f87107d
Kaikkitietokoneista.net <info@kaikkitietokoneista.net>
2020-12-18 21:06:01 +0200

    Made Github pages

--- test.js
+++
@@ -1,8 +0,0 @@
-const koulusafkapi = require('./kouluruoka-api');
-
-async function main() {
-  console.log(await koulusafkapi.getSchools())
-  console.log(await koulusafkapi.foodInSchool('80b1081a-720c-eb11-8143-00215a9b975d'))
-}
-
-main()
